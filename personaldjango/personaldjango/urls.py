5f7f97659a0a0b2f328b426c4ae18b8bf352a3b9
Danny McGrory <dannyalaska@mac.com>
2016-02-10 13:53:38 -0500

    fixed image issues, created static assets for css and js

--- personaldjango/personaldjango/urls.py
+++ personaldjango/personaldjango/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 from personalapp import views
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^project/(?P<id>\d+)/', views.project_detail, name='project_detail'),
     url(r'^admin/', admin.site.urls),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

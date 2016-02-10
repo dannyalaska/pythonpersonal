fd7d94c428904227cb8138843142616295491584
Danny McGrory <dannyalaska@mac.com>
2016-02-10 11:44:33 -0500

    got templates up, images not showing up

--- personaldjango/personalapp/views.py
+++ personaldjango/personalapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+from personalapp.models import Project
 
 def index(request):
-    return HttpResponse('<p>Made it to index view</p>')
+    projects = Project.objects.all()
+    return render(request, 'projects/index.html', {
+        'projects': projects,
+    })
 
 def project_detail(request, id):
-    return HttpResponse('<p>In project_detail view with id {0}</p>'.format(id))
+    try:
+        project = Project.objects.get(id=id)
+    except Project.DoesNotExist:
+        raise Http404('This project does not exist')
+    return render(request, 'projects/project_detail.html', {
+        'project': project,
+    })
--- personaldjango/personaldjango/settings.py
+++ personaldjango/personaldjango/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'personaldjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['personaldjango/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/path/to/static/folder/static'

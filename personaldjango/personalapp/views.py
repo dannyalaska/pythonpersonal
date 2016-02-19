c99250d9de7200e3122c1c15c533cb362ffcf05c
Danny McGrory <dannyalaska@mac.com>
2016-02-19 11:50:06 -0500

    working on hero images

--- personaldjango/personalapp/views.py
+++ personaldjango/personalapp/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import Http404
 
 from personalapp.models import Project
+from personalapp.models import Splash
 
 def index(request):
+    splashes = Splash.objects.all()
     projects = Project.objects.all()
     return render(request, 'projects/index.html', {
         'projects': projects,
+        'splashes': splashes,
     })
 
 def project_detail(request, id):
@@ -17,3 +20,6 @@ def project_detail(request, id):
     return render(request, 'projects/project_detail.html', {
         'project': project,
     })
+
+def splash_list(request, id):
+    splash = Project.objects.get(id=id)

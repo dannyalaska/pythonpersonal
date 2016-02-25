114e95f5044b48a0d992ab6ed9c153ed3648c315
Danny McGrory <dannyalaska@mac.com>
2016-02-25 08:51:08 -0500

    fixing wsgi

--- personaldjango/personaldjango/wsgi.py
+++ personaldjango/personaldjango/wsgi.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 # assuming your django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/myusername/mysite'
+path = '/home/dannyalaska/personaldjango'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'personaldjango.settings'
 
 ## Uncomment the lines below depending on your Django version
 ###### then, for django >=1.5:
